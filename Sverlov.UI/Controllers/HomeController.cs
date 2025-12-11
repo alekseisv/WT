da5756bfb531b7cea5c92dcc3c49fb8ff873c4c7
Aleksei Sv <alekseisv@mail.ru>
2025-12-11 11:56:21 +0300

    Добавьте файлы проекта.

---
+++ Sverlov.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sverlov.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Greeting"] = "Лабораторная работа №2";
+            return View();
+        }
+    }
+}
---
+++ Sverlov.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sverlov.UI.ViewComponents;
+
+public class CartViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();                      
+    }
+}
