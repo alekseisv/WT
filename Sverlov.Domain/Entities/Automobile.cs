12d516d2e3eaf88df403b53a61684f95e3d5048c
Aleksei Sv <alekseisv@mail.ru>
2025-12-11 12:56:22 +0300

    The third comment

---
+++ Sverlov.Domain/Entities/Automobile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sverlov.Domain.Entities;
+
+public class Automobile
+{
+    public int Id { get; set; } // id авто
+    public string Name { get; set; } // марка машины
+    public string Description { get; set; } // описание машины
+    public int LiftingCapacity { get; set; } // грузоподъмность
+    public string? Image { get; set; } // путь к файлу изображения
+                                       // Навигационные свойства
+    /// <summary>
+    /// группа транспортных средств (например, легковые, грузовые и т.д.)
+    /// </summary>
+    public int TheTransportTypeId { get; set; }
+    public TheTransportType? TheTransportType { get; set; }
+}
